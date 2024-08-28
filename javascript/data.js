30b006969a0f58487f3636f3a97941a1ca80a020
BarshaArpita <arpitalenka03@gmail.com>
2024-08-28 20:44:20 +0530

    add data.js file

---
+++ javascript/data.js
@@ -0,0 +1,56 @@
+// let name;
+//name = "Barsha";
+//console.log(name);//Barsha
+
+// let num;
+// num = "null";
+// console.log(num);//null
+
+// let name1;
+// name1 = undefined;
+// console.log(name1);//undefined
+
+// let num1 = 100;
+// console.log(num1);//100
+
+// let Num = 100;
+//console.log(Num);//100
+
+//num !== Num (case sensitive)
+
+
+
+//Task-1 :
+//Do a mathematical operation to perform addition,substraction,multiplication and division using "number" datatypes in js .
+
+//Addition:
+// let num1 = 100;
+// let num2 = 200;
+// let result = num1 + num2;
+// console.log(result);//300
+
+//Substraction:
+// let num1 = 200;
+// let num2 = 50;
+// let sub_result = num1-num2;
+// console.log(sub_result); //150
+
+//Multiplication:
+// let num1 = 78;
+// let num2 = 89;
+// let multiplication_result = num1 * num2;
+// console.log(multiplication_result); //6942
+
+//Division :
+let num1 = 240;
+let num2 = 40;
+let division_result = num1 / num2;
+console.log(division_result);//6
+
+
+
+
+
+
+
+
