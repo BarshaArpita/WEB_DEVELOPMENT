f61e691c1a8e935115f19faef55c2f6501890efc
BarshaArpita <arpitalenka03@gmail.com>
2024-12-09 17:36:51 +0530

    react updated

---
+++ react/class-10/mems-project/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Mems from "./components/Mems";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center mb-7 underline">
+        Memes Application
+      </h1>
+      <Mems />
+    </>
+  );
+}
+
+export default App;
